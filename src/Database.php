f50869b99ebf73a45584a2bc3b3b51a0ff7bdeb1
karacalla <caracallam@gmail.com>
2026-03-27 09:34:39 +0100

    fix: add Singleton pattern and missing $pass validation

--- src/Database.php
+++ src/Database.php
@@ -9,30 +9,37 @@ use PDOException;
 
 class Database
 {
+    private static ?PDO $instance = null; // création du singleton
+
     public static function getConnection(): PDO
     {
-        try {
-            $host = $_ENV['DB_HOST'] ?? '';
-            $port = $_ENV['DB_PORT'] ?? '';
-            $dbname = $_ENV['DB_NAME'] ?? '';
-            $user = $_ENV['DB_USER'] ?? '';
-            $pass = $_ENV['DB_PASS'] ?? '';
+        if (self::$instance === null) { //crée l'instance une seule fois
+            try {
+                $host = $_ENV['DB_HOST'] ?? '';
+                $port = $_ENV['DB_PORT'] ?? '';
+                $dbname = $_ENV['DB_NAME'] ?? '';
+                $user = $_ENV['DB_USER'] ?? '';
+                $pass = $_ENV['DB_PASS'] ?? '';
 
-            if (!$host || !$port || !$dbname || !$user) {
-                die('Erreur : variables .env manquantes');
+                if (!$host || !$port || !$dbname || !$user || !$pass) {
+                    die('Erreur : variables .env manquantes');
+                }
+
+                $dsn = "mysql:host={$host};port={$port};dbname={$dbname};charset=utf8mb4";
+
+                $pdo = new PDO($dsn, $user, $pass);
+
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+                self::$instance = $pdo; // singleton
+
+            } catch (PDOException $e) {
+                die('Erreur connexion DB : ' . $e->getMessage());
             }
 
-            $dsn = "mysql:host={$host};port={$port};dbname={$dbname};charset=utf8mb4";
-
-            $pdo = new PDO($dsn, $user, $pass);
-
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-            return $pdo;
-
-        } catch (PDOException $e) {
-            die('Erreur connexion DB : ' . $e->getMessage());
         }
+
+        return self::$instance; // si l'instance existe déjà
     }
 }
